f928e64776ce20869c861ba074b69ab52776390d
Yuta Suzuki <bugs_bugs_bunny69@yahoo.co.jp>
2017-09-21 08:06:21 +0900

    update

--- src/js/battle/battle.js
+++ src/js/battle/battle.js
@@ -1,13 +1,9 @@
 class Battle {
   constructor() {
     this.stage = {};
-    this.command = {
-      attack: new createjs.Shape(),
-      magic: new createjs.Shape(),
-      skill: new createjs.Shape(),
-      skip: new createjs.Shape(),
-    };
-    const preload = new createjs.LoadQueue(false);
+    this.command = {};
+    const preload = new createjs.LoadQueue();
+    const loaders = {};
     const fieldManifest = [
       {src: "forest1.jpg", id: "forest1"},
     ];
@@ -19,31 +15,32 @@ class Battle {
     ];
     preload.loadManifest(commandManifest, true, "/assets/images/battle/");
     preload.loadManifest(fieldManifest, true, "/assets/images/field/");
-    preload.addEventListener('complete', () => {
-      this.stage = new createjs.Stage("content");
-      const w = this.stage.canvas.width;
-      const h = this.stage.canvas.height;
-      const field = new createjs.Shape();
-      field.graphics.beginBitmapFill(preload.getResult("forest1")).drawRect(0, 0, w, h);
-      this.command.attack.graphics.beginBitmapFill(preload.getResult("attack")).drawRect(0, 0, w, 200);
-      this.command.attack.scaleX = 0.5;
-      this.command.attack.scaleY = 0.15;
-      this.command.magic.graphics.beginBitmapFill(preload.getResult("magic")).drawRect(0, 0, w, h);
-      this.command.skill.graphics.beginBitmapFill(preload.getResult("skill")).drawRect(0, 0, w, h);
-      this.command.skip.graphics.beginBitmapFill(preload.getResult("skip")).drawRect(0, 0, w, h);
-      this.stage.addChild(field, this.command.attack);
+    preload.addEventListener('fileload', (e) => {
+      loaders[e.item.id] = e.result;
+      console.log(loaders);
+    });
+    preload.addEventListener('complete', (e) => {
+      console.log(e.result);
+      this.stage = new createjs.Stage('content');
+      this.command = {
+        attack: new createjs.Bitmap(loaders['attack']),
+        magic: new createjs.Bitmap(loaders['magic']),
+        skill: new createjs.Bitmap(loaders['skill']),
+        skip: new createjs.Bitmap(loaders['skip']),
+      };
+      this.stage.addChild(new createjs.Bitmap(loaders['forest1']));
       this.stage.update();
       this.init();
     });
   }
 
   init() {
-    // const circle = new createjs.Shape();
-    // circle.graphics.beginFill("DeepSkyBlue").drawCircle(0, 0, 50);
-    // circle.x = 100;
-    // circle.y = 100;
-    // this.stage.addChild(circle);
-    // this.stage.update();
+    const circle = new createjs.Shape();
+    circle.graphics.beginFill("DeepSkyBlue").drawCircle(0, 0, 50);
+    circle.x = 100;
+    circle.y = 100;
+    this.stage.addChild(circle);
+    this.stage.update();
   }
 
   handleFileComplete(event) {
